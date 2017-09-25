e2fd5c4a136774fa389b9ef2bd85202f7115b895
linus <linus@Linus-Dator>
2017-09-25 23:56:27 +0200

    Lägger till en testapplikation som jag använder för att utveckla en klass för att göra HTTP requests.

---
+++ AppGuitarOffical/app/src/main/java/se/guitar_project/miun/appguitaroffical/XmlRestInterface.java
@@ -0,0 +1,67 @@
+package se.guitar_project.miun.appguitaroffical;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by linus on 2017-09-25.
+ */
+
+public class XmlRestInterface extends AsyncTask<String, Intent, String> {
+
+    public interface AsyncResponse
+    {
+        void processFinish(String output);
+    }
+    public AsyncResponse delegate = null;
+    public XmlRestInterface(AsyncResponse delegate){
+        this.delegate = delegate;
+    }
+
+    @Override
+    protected String doInBackground(String... urls)
+    {
+        String content = "";
+        try
+        {
+            int size = urls.length;
+            URL url = null;
+            for (int i = 0; i < size; i++)
+            {
+                url = new URL(urls[i]);
+                content = getContentFromHttp(url);
+            }
+        }
+        catch(Exception e)
+        {
+            content = e.toString();
+        }
+        return content;
+    }
+    @Override
+    protected void onPostExecute(String string)
+    {
+        delegate.processFinish(string);
+    }
+    private String getContentFromHttp(URL url) throws IOException
+    {
+        String content = "";
+        HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+        InputStream input = connection.getInputStream();
+        InputStreamReader inputReader = new InputStreamReader(input);
+
+        int temp = 0;
+        while((temp = inputReader.read()) != -1)
+        {
+            content += (char)temp;
+        }
+        return content;
+    }
+}
