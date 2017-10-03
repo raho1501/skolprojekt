951e9cbd4b5c46dc742a8d44728a52c57a7ebb69
Limeman <Loepolepe@gmail.com>
2017-10-03 12:58:26 +0200

    Lagt till nuvarande vecka i titel för weekview
    sdfhjksdjlksdjklsdjlksjlkd

--- AlphaGuitarrprojectSprint2/app/src/main/java/com/example/limeman/alphaguitarrprojectsprint2/WeekView.java
+++ AlphaGuitarrprojectSprint2/app/src/main/java/com/example/limeman/alphaguitarrprojectsprint2/WeekView.java
@@ -1,16 +1,30 @@
 package com.example.limeman.alphaguitarrprojectsprint2;
 
 import android.app.Activity;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
+import android.icu.util.GregorianCalendar;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 /**
  * Created by Limeman on 10/3/2017.
  */
 
 public class WeekView extends Activity {
+
+    String currWeek;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.week_view);
+        final Calendar c = Calendar.getInstance();
+        TextView tv = (TextView)findViewById(R.id.textView1);
+
+        currWeek = "Vecka " + Integer.toString(c.WEEK_OF_YEAR);
+
+        tv.setText(currWeek);
     }
 }
