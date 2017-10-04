cf2be61dbb15b6ccdf9cbe157cb930b1333238dc
Zorac23 <mayr1400@gmail.com>
2017-10-04 17:13:40 +0200

    Dagschema uppdaterad.
    Felaktig startsida, klicka på dagsschema för att komma till uppdaterad version av dagschema med swipes+tabs.

--- AlphaGuitarrprojectSprint2/app/src/main/java/com/example/limeman/alphaguitarrprojectsprint2/MainActivity.java
+++ AlphaGuitarrprojectSprint2/app/src/main/java/com/example/limeman/alphaguitarrprojectsprint2/MainActivity.java
@@ -96,7 +96,11 @@ public class MainActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_send) {
 
-        }
+        } else if (id == R.id.dayView) {
+
+            Intent i = new Intent(this, DayView.class);
+            startActivity(i);
+    }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
