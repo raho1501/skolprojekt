a6aa01fbff5d72a3513eb0d717a49c7579fe9ce0
Markus Fält <markus.falt@gmail.com>
2017-10-06 13:39:33 +0200

    La till webb sidan i netbeans.

--- web_guitar_official/build/generated-sources/ap-source-output/beans/Leave_.java
+++ web_guitar_official/build/generated-sources/ap-source-output/beans/Leave_.java
@@ -5,7 +5,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-05T23:06:12")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-06T13:38:16")
 @StaticMetamodel(Leave.class)
 public class Leave_ { 
 
