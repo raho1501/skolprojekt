4c6020a0a42454c4983231a16d066b9187b37527
Markus Fält <markus.falt@gmail.com>
2017-10-06 22:43:34 +0200

    Ändrar boknings sidan från xhtml till jsp.

--- web_guitar_official/build/generated-sources/ap-source-output/beans/TimeReservation_.java
+++ web_guitar_official/build/generated-sources/ap-source-output/beans/TimeReservation_.java
@@ -9,7 +9,7 @@ import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-06T13:38:16")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-10-06T22:39:59")
 @StaticMetamodel(TimeReservation.class)
 public class TimeReservation_ { 
 
