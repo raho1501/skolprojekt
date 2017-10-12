1eed4017188c6f038bc073b98f79771a6de16d16
Limeman <Loepolepe@gmail.com>
2017-10-12 14:03:24 +0200

    Added Events class

---
+++ Hamburgermenu/app/src/main/java/hamburgermenu/demo/fragments/Events.java
@@ -0,0 +1,14 @@
+package hamburgermenu.demo.fragments;
+
+import java.util.ArrayList;
+
+/**
+ * Created by Limeman on 10/12/2017.
+ */
+
+public class Events {
+    private ArrayList<Event> events;
+    public ArrayList<Event> getEvents(){
+        return  events;
+    }
+}
