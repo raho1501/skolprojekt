a830cd5c62ae5f9f632c7ceac1e02907a5b6bd57
raho <raho1501@student.miun.se>
2017-10-12 13:38:46 +0200

    no date fragment, yes spinners
    bytt datepickers mot spinners

--- Hamburgermenu/app/src/main/java/hamburgermenu/demo/fragments/Bokahandelse.java
+++ Hamburgermenu/app/src/main/java/hamburgermenu/demo/fragments/Bokahandelse.java
@@ -1,26 +1,109 @@
 package hamburgermenu.demo.fragments;
 
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.markus.hamburgermenu.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Markus on 2017-10-11.
  */
 
 public class Bokahandelse extends Fragment {
+    private String[] arraySpinner;
+
 
     @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
+                                                        @Nullable Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.bokahandelse, container, false);
 
         return rootView;
     }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Calendar c = Calendar.getInstance();
+        TextView tv = (TextView)getView().findViewById(R.id.inputBoxName);
+        //create desired formatting for displaying the date
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        tv.setText(sdf.format(c.getTime()));
+
+        Spinner monthSpinner = (Spinner) getView().findViewById(R.id.monthSpinner);
+        Spinner dateSpinner = (Spinner) getView().findViewById(R.id.datumSpinner);
+        Spinner startTidSpinner = (Spinner) getView().findViewById(R.id.startTidSpinner);
+        Spinner stopTidSpinner = (Spinner) getView().findViewById(R.id.stopTidSpinner);
+
+
+        ArrayAdapter<String> monthAdapter;
+        List<String> list;
+
+        list = new ArrayList<String>();
+        List<String> tidList;
+        tidList = new ArrayList<String>();
+        for(int i = 1;i<=12;i++)
+        {
+            list.add(String.valueOf(i));
+        }
+        for(int j = 10; j <=18;j++)
+        {
+            String t = String.valueOf(j);
+            t= t+":00";
+            tidList.add(t);
+            t="";
+        }
+        monthAdapter = new ArrayAdapter<String>(this.getContext(),
+                android.R.layout.simple_spinner_item, list);
+        monthAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        monthSpinner.setAdapter(monthAdapter);
+
+
+        ArrayAdapter<String> dateAdapter= new ArrayAdapter<String>(this.getContext(),
+                android.R.layout.simple_spinner_item, list);
+        dateAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        dateSpinner.setAdapter(dateAdapter);
+
+        ArrayAdapter<String> startTidAdapter= new ArrayAdapter<String>(this.getContext(),
+                android.R.layout.simple_spinner_item, tidList);
+        startTidAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        startTidSpinner.setAdapter(startTidAdapter);
+
+        ArrayAdapter<String> stopTidAdapter= new ArrayAdapter<String>(this.getContext(),
+                android.R.layout.simple_spinner_item, tidList);
+        stopTidAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        stopTidSpinner.setAdapter(stopTidAdapter);
+
+
+
+        View view = getView();
+        if(view != null) {
+            view.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    System.out.println("click");
+                }
+            });
+        }
+    }
+
+
+
 }
