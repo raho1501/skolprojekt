affcdc4c8cb42b72a66efe51aab09e8906eff819
Linus <linus96@hotmail.se>
2017-10-16 16:36:04 +0200

    Fixade bugget där alla Event innan 10 på morognen visades 10 på kvällen.

--- CalenderTest/app/src/main/java/se/guitar_project/miun/calendertest/Event.java
+++ CalenderTest/app/src/main/java/se/guitar_project/miun/calendertest/Event.java
@@ -8,6 +8,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 /**
  * Created by linus on 2017-10-11.
@@ -62,15 +63,15 @@ public class Event
         WeekViewEvent weekViewEvent = new WeekViewEvent();
 
         Calendar now = Calendar.getInstance();
-        SimpleDateFormat hourMinutes = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
-        SimpleDateFormat years = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+        SimpleDateFormat parser = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm");
+
         Calendar start = (Calendar) now.clone();
 
         Calendar stop = (Calendar) now.clone();
         Calendar reservationDate = (Calendar) now.clone();
         try
         {
-            Date temp = hourMinutes.parse(getStartTime());
+            Date temp = parser.parse(getStartTime());
             start.setTime(temp);
         }
         catch(ParseException e)
@@ -80,7 +81,7 @@ public class Event
 
         try
         {
-            Date temp = hourMinutes.parse(getStopTime());
+            Date temp = parser.parse(getStopTime());
             stop.setTime(temp);
         }
         catch(ParseException e)
@@ -89,7 +90,7 @@ public class Event
         }
         try
         {
-            Date temp = years.parse(getDate());
+            Date temp = parser.parse(getDate());
             reservationDate.setTime(temp);
         }
         catch(ParseException e)
@@ -102,14 +103,14 @@ public class Event
         Calendar startTime = (Calendar) now.clone();
         startTime.set(Calendar.YEAR, reservationDate.get(Calendar.YEAR));
         startTime.set(Calendar.DATE, reservationDate.get(Calendar.DATE));
-        startTime.set(Calendar.HOUR, start.get(Calendar.HOUR));
+        startTime.set(Calendar.HOUR_OF_DAY, start.get(Calendar.HOUR_OF_DAY));
         startTime.set(Calendar.MINUTE, start.get(Calendar.MINUTE));
         startTime.set(Calendar.SECOND, start.get(Calendar.SECOND));
 
         Calendar stopTime = (Calendar) now.clone();
         stopTime.set(Calendar.YEAR, reservationDate.get(Calendar.YEAR));
         stopTime.set(Calendar.DATE, reservationDate.get(Calendar.DATE));
-        stopTime.set(Calendar.HOUR, stop.get(Calendar.HOUR));
+        stopTime.set(Calendar.HOUR_OF_DAY, stop.get(Calendar.HOUR_OF_DAY));
         stopTime.set(Calendar.MINUTE, stop.get(Calendar.MINUTE));
         stopTime.set(Calendar.SECOND, stop.get(Calendar.SECOND));
 
--- CalenderTest/app/src/main/java/se/guitar_project/miun/calendertest/MainActivity.java
+++ CalenderTest/app/src/main/java/se/guitar_project/miun/calendertest/MainActivity.java
@@ -5,15 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.alamkanak.weekview.DateTimeInterpreter;
 import com.alamkanak.weekview.MonthLoader;
 import com.alamkanak.weekview.WeekView;
 import com.alamkanak.weekview.WeekViewEvent;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Vector;
 
 public class MainActivity extends AppCompatActivity implements WeekView.EventClickListener, MonthLoader.MonthChangeListener, WeekView.EventLongPressListener{
@@ -27,6 +30,7 @@ public class MainActivity extends AppCompatActivity implements WeekView.EventCli
         // Get a reference for the week view in the layout.
         final WeekView weekView = (WeekView) findViewById(R.id.weekView);
 
+
         // Set an action when any event is clicked.
         weekView.setOnEventClickListener(this);
 
@@ -39,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements WeekView.EventCli
 
         //Set the number of visible days.
         weekView.setNumberOfVisibleDays(5);
+
         //Event event = new Event();
         //event.setName("Event");
         //event.setDate("10/12/2017");
