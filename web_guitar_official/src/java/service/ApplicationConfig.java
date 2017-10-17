7b905d662f435747d3d81e414103acbe0590e6e2
Linus <linus96@hotmail.se>
2017-10-17 10:36:14 +0200

    Mergade calendarTest och androidAlpha

--- web_guitar_official/src/java/service/ApplicationConfig.java
+++ web_guitar_official/src/java/service/ApplicationConfig.java
@@ -1,41 +1,3 @@
-<<<<<<< HEAD
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package service;
-
-import java.util.Set;
-import javax.ws.rs.core.Application;
-
-/**
- *
- * @author linus
- */
-@javax.ws.rs.ApplicationPath("webresources")
-public class ApplicationConfig extends Application {
-
-    @Override
-    public Set<Class<?>> getClasses() {
-        Set<Class<?>> resources = new java.util.HashSet<>();
-        addRestResourceClasses(resources);
-        return resources;
-    }
-
-    /**
-     * Do not modify addRestResourceClasses() method.
-     * It is automatically populated with
-     * all resources defined in the project.
-     * If required, comment out calling this method in getClasses().
-     */
-    private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(service.AppointmentFacadeREST.class);
-        resources.add(service.CustomerFacadeREST.class);
-    }
-    
-}
-=======
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -75,4 +37,3 @@ public class ApplicationConfig extends Application {
     }
     
 }
->>>>>>> CalenderTest
