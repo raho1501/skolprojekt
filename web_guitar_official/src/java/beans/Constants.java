ce5d7ca44cb08e0564d23cd78e17b94a0aadf7ee
Markus Fält <markus.falt@gmail.com>
2017-10-23 22:52:10 +0200

    Nu ska Anders och kunden få ett mail så att de kan ta bort reservation när en reservation sker.

--- web_guitar_official/src/java/beans/Constants.java
+++ web_guitar_official/src/java/beans/Constants.java
@@ -12,4 +12,7 @@ package beans;
 public class Constants {
 	public static final String uploadPath =
 		"/home/markus/NetBeansProjects/skolprojekt/web_guitar_official/web/resources/uploaded_img";
+	public static final String mailAcount = "andersgitarrautomail";
+	public static final String mailPassword = "Frågra Markus F om lösen till andersgitarrautomail";
+	public static final String toMail = "Byt denna text mot Anders mail address.";
 }
